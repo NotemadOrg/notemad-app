aaadb5f208f00e8e4d526b08699621bbe260a621
qhe1332 <cherryhe5@gmail.com>
2022-12-08 12:09:11 -0500

    displayable map

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -4,12 +4,12 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 function MapView(prop)
 {
     return(
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[40.71, -73.93]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[40.71, -73.93]}>
             <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
